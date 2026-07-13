1faf40f57d001e064a446789e6b66415712994b1
sofiene rouatbi <sofiene.rouatbi@medtech.tn>
2026-07-13 14:36:10 -0400

    test frontend CI

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -17,7 +17,7 @@ import './styles/global.css';
 const queryClient = new QueryClient({
   defaultOptions: { queries: { retry: 1, refetchOnWindowFocus: false } },
 });
-
+ 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
